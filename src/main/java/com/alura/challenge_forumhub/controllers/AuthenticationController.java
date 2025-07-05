0e585f360410d6a2208e8344ce975d8642ef7b51
erikmarconato <erik.marconato@hotmail.com>
2025-07-05 05:13:53 -0300

    refactor: remove unused @Autowired import in AuthenticationController

--- src/main/java/com/alura/challenge_forumhub/controllers/AuthenticationController.java
+++ src/main/java/com/alura/challenge_forumhub/controllers/AuthenticationController.java
@@ -6,7 +6,6 @@ import com.alura.challenge_forumhub.dtos.RegisterUserDto;
 import com.alura.challenge_forumhub.services.AuthenticationService;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
